b1e373de0b4acdea87d22f2adf96a2cbff88177a
Novikova Olesya <lessik1511@rambler.ru>
2023-06-11 16:53:25 +0300

    iter4 edits 6

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -12,7 +12,7 @@ var reportInterval time.Duration
 
 // parseFlags обрабатывает аргументы командной строки
 func parseFlags() {
-	flag.StringVar(&flagAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagAddr, "a", "localhost:8080", "address and port to run server")
 	flag.DurationVar(&pollInterval, "p", 2*time.Second, "metric collection interval")
 	flag.DurationVar(&reportInterval, "r", 10*time.Second, "metrics sending interval")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
