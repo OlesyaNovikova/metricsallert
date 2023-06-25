ed4998bf731bd17b59f95b004c2d70ecec5cf5dc
Novikova Olesya <lessik1511@rambler.ru>
2023-06-25 15:47:55 +0300

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,25 +4,38 @@ import (
 	"net/http"
 
 	chi "github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 
 	h "github.com/OlesyaNovikova/metricsallert.git/internal/handlers"
 	s "github.com/OlesyaNovikova/metricsallert.git/internal/storage"
 )
 
+var sugar zap.SugaredLogger
+
 func main() {
 
 	parseFlags()
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar = *logger.Sugar()
 
 	mem := s.NewStorage()
 	h.NewMemRepo(&mem)
 
 	r := chi.NewRouter()
-	r.Post("/update/{memtype}/{name}/{value}", h.UpdateMem)
-	r.Get("/value/{memtype}/{name}", h.GetMem)
-	r.Get("/", h.GetAllMems)
+	r.Post("/update/{memtype}/{name}/{value}", WithLogging(h.UpdateMem()))
+	r.Get("/value/{memtype}/{name}", WithLogging(h.GetMem()))
+	r.Get("/", WithLogging(h.GetAllMems()))
 
-	err := http.ListenAndServe(flagAddr, r)
+	sugar.Infow("Starting server", "addr", flagAddr)
+
+	err = http.ListenAndServe(flagAddr, r)
 	if err != nil {
+		sugar.Fatalw(err.Error(), "event", "start server")
 		panic(err)
 	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -27,70 +27,76 @@ func NewMemRepo(Mem MemDataBase) {
 	}
 }
 
-func UpdateMem(res http.ResponseWriter, req *http.Request) {
-	fmt.Print("Run updateMem:\n")
-	if req.Method != http.MethodPost {
-		fmt.Print("Only POST requests are allowed!\n")
-		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
+func UpdateMem() http.Handler {
+	fn := func(res http.ResponseWriter, req *http.Request) {
 
-	memtype := chi.URLParam(req, "memtype")
-	name := chi.URLParam(req, "name")
-	value := chi.URLParam(req, "value")
-
-	switch memtype {
-	case "gauge":
-		val, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			fmt.Println("BadRequest-meaning")
-			res.WriteHeader(http.StatusBadRequest)
+		if req.Method != http.MethodPost {
+			fmt.Print("Only POST requests are allowed!\n")
+			http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 			return
 		}
-		memBase.S.UpdateGauge(name, val)
-		res.WriteHeader(http.StatusOK)
-		return
 
-	case "counter":
-		val, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			fmt.Println("BadRequest-meaning")
-			res.WriteHeader(http.StatusBadRequest)
+		memtype := chi.URLParam(req, "memtype")
+		name := chi.URLParam(req, "name")
+		value := chi.URLParam(req, "value")
+
+		switch memtype {
+		case "gauge":
+			val, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				fmt.Println("BadRequest-meaning")
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			memBase.S.UpdateGauge(name, val)
+			res.WriteHeader(http.StatusOK)
+			return
+
+		case "counter":
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				fmt.Println("BadRequest-meaning")
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			memBase.S.UpdateCounter(name, val)
+			res.WriteHeader(http.StatusOK)
 			return
 		}
-		memBase.S.UpdateCounter(name, val)
-		res.WriteHeader(http.StatusOK)
-		return
-	}
-	fmt.Println("BadRequest-type")
-	res.WriteHeader(http.StatusBadRequest)
-}
-
-func GetMem(res http.ResponseWriter, req *http.Request) {
-	fmt.Print("Run getMem:\n")
-	if req.Method != http.MethodGet {
-		fmt.Print("Only GET requests are allowed!\n")
-		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	memtype := chi.URLParam(req, "memtype")
-	name := chi.URLParam(req, "name")
-
-	strValue, err := memBase.S.GetString(name, memtype)
-
-	if err != nil {
 		fmt.Println("BadRequest-type")
 		res.WriteHeader(http.StatusBadRequest)
-		return
 	}
-	if strValue == "" {
-		fmt.Println("Metric not found")
-		res.WriteHeader(http.StatusNotFound)
-		return
-	}
+	return http.HandlerFunc(fn)
+}
 
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
-	res.Write([]byte(strValue))
+func GetMem() http.Handler {
+	fn := func(res http.ResponseWriter, req *http.Request) {
+
+		if req.Method != http.MethodGet {
+			fmt.Print("Only GET requests are allowed!\n")
+			http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		memtype := chi.URLParam(req, "memtype")
+		name := chi.URLParam(req, "name")
+
+		strValue, err := memBase.S.GetString(name, memtype)
+
+		if err != nil {
+			fmt.Println("BadRequest-type")
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if strValue == "" {
+			fmt.Println("Metric not found")
+			res.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		res.Header().Set("Content-Type", "text/plain")
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte(strValue))
+	}
+	return http.HandlerFunc(fn)
 }
